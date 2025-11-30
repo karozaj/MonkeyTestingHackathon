cd20c8176c67898f4f8ef5db98bf8888d8249e4b
karozaj <135151900+karozaj@users.noreply.github.com>
2025-11-30 04:05:47 +0100

    detail view

--- app/src/main/java/com/example/monkeytestinghackathon/ui/views/EventDetailView.kt
+++ app/src/main/java/com/example/monkeytestinghackathon/ui/views/EventDetailView.kt
@@ -1,11 +1,17 @@
 package com.example.monkeytestinghackathon.ui.views
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -39,9 +45,30 @@ fun EventDetailView(
                             .padding(8.dp)
                     ) {
                         Text("Title: ")
+                        DefaultSpacer()
                         Text("Description: ")
-                        Text("Star date: ")
-                        Text("End date:")
+                        DefaultSpacer()
+                        Text("Game: ")
+                        DefaultSpacer()
+                        Text("Type: ")
+                        DefaultSpacer()
+                        Text("Location: ")
+                        DefaultSpacer()
+                        Text("Start date: ")
+                        DefaultSpacer()
+                        Text("End date: ")
+                        DefaultSpacer()
+                        Text("Participants: ")
+                        DefaultSpacer()
+                        Row(
+                            Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ){
+                            Button(onClick = {}) { Text("Join event") }
+                            Button(onClick = {}) { Text("Leave event") }
+                        }
+
+
                     }
                 }
             }
